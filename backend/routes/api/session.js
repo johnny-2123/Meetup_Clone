de1f9bf213a1f1acfc1ae2cd08a8d7ae7db25afa
edgar <johnnyavila2021@u.northwestern.edu>
2023-01-14 14:17:44 -0500

    Add a backend endpoint to get the current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -28,6 +28,18 @@ router.post(
     }
 );
 
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            return res.json({
+                user: user.toSafeObject()
+            });
+        } else return res.json({});
+    }
+)
 router.delete(
     '/',
     (req, res) => {
