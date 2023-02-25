ed52e6363b39a6a6caaa4b664c625eb9c213f1fd
edgar <johnnyavila2021@u.northwestern.edu>
2023-02-25 16:34:57 -0500

    fix bug in frontend path_

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'production') {
     router.get(/^(?!\/?api).*/, (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
         return res.sendFile(
-            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+            path.resolve(__dirname, '../../frontend', 'index.html')
         );
     });
 }
