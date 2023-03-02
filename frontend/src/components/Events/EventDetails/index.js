d4430f91c9dbe3ee5158efe89b48d310041ead4e
edgar <johnnyavila2021@u.northwestern.edu>
2023-03-02 18:02:01 -0500

    adding information and css styling to eventDetails component

--- frontend/src/components/Events/EventDetails/index.js
+++ frontend/src/components/Events/EventDetails/index.js
@@ -5,7 +5,7 @@ import AlertConfirm from 'react-alert-confirm';
 import * as eventActions from '../../../store/events'
 import * as sessionActions from '../../../store/session'
 import './EventDetails.css'
-
+<script src="https://kit.fontawesome.com/97726b2eee.js" crossorigin="anonymous"></script>
 
 function EventDetailsComponent() {
     const { eventId } = useParams();
@@ -30,31 +30,33 @@ function EventDetailsComponent() {
 
     return (
         loaded && <div className='mainGroupDetailsDiv'>
-            <div className='eventDetailsTopSection'>
-                <button>{`< Events`}</button>
-                <h1>{event?.name}</h1>
-                <h2>Hosted By {event?.Organizer?.firstName} {event?.Organizer?.lastName}</h2>
+            <div className='eventDetailsTopSectionContainer'>
+                <div className='eventDetailsTopSection'>
+                    <button className='backButton'>{`< Events`}</button>
+                    <h1>{event?.name}</h1>
+                    <h2>Hosted By {event?.Organizer?.firstName} {event?.Organizer?.lastName}</h2>
+                </div>
             </div>
-            <div>
-                <div>
-                    <img src='https://images.unsplash.com/photo-1517457373958-b7bdd4587205?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZXZlbnR8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60' />
-                    <div>
+            <div className='eventDetailsGreySection'>
+                <div className='eventDetailsMiddleSection'>
+                    <div className='eventDetailsGreySectionTopHalf'>
+                        <img className='eventDetailsImage' src='https://images.unsplash.com/photo-1517457373958-b7bdd4587205?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZXZlbnR8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60' />
                         <div>
-                            <img src='https://res.cloudinary.com/dkul3ouvi/image/upload/v1677439417/5498791_i3opa9.jpg' />
+                            <div className='eventDetailsGroupDiv'>
+                                <img className='eventDetailsGroupImage' src='https://res.cloudinary.com/dkul3ouvi/image/upload/v1677439417/5498791_i3opa9.jpg' />
+                                <div>
+                                    <h3>{event?.Group?.name}</h3>
+                                    <h4>{event.Group?.private ? `private` : `public`}</h4>
+                                </div>
+                            </div>
                             <div>
-                                <h3>{event.Group.name}</h3>
-                                <h4>{event.Group.private ? `private` : `public`}</h4>
+                                <div>
+                                    <i class="fa-regular fa-clock"></i>
+                                </div>
                             </div>
                         </div>
-                        <div>
-                            <div>
-                                <button>
-                                    <i class="fa-thin fa-clock"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
 
+                    </div>
                 </div>
             </div>
         </div>
