17b7edd7ce5ac7a393dc326cb862e267e922d2c0
edgar <johnnyavila2021@u.northwestern.edu>
2023-07-21 10:42:40 -0400

    add animated underline to session link buttons on hover

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,72 +1,91 @@
-import React, { useReducer } from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import './Navigation.css';
-import * as sessionActions from '../../store/session';
-import { useHistory } from 'react-router';
+import React, { useReducer } from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
+import * as sessionActions from "../../store/session";
+import { useHistory } from "react-router";
 
-import { useModal } from '../../context/Modal';
+import { useModal } from "../../context/Modal";
 function Navigation({ isLoaded }) {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const sessionUser = useSelector(state => state.session.user?.user);
-    const currentUserGroups = useSelector(state => state.groups.currentUserGroups)
-    let sessionLinks;
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const sessionUser = useSelector((state) => state.session.user?.user);
+  const currentUserGroups = useSelector(
+    (state) => state.groups.currentUserGroups
+  );
+  let sessionLinks;
 
-    const { setShowLogin, setShowSignUp } = useModal()
-    const [, forceUpdate] = useReducer(x => x + 1, 0);
+  const { setShowLogin, setShowSignUp } = useModal();
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
 
-    const loginButton = () => {
-        setShowLogin(true)
-    }
-    const signUpButton = () => {
-        setShowSignUp(true)
-    }
+  const loginButton = () => {
+    setShowLogin(true);
+  };
+  const signUpButton = () => {
+    setShowSignUp(true);
+  };
 
-    const handleDemoLoginClick = () => {
-        return dispatch(sessionActions.fetchLogin({ credential: 'demoEdgar', password: 'password' }))
-            .then(() => history.push('/'))
-            .then(() => window.location.reload());
-
-    }
-    if (sessionUser && sessionUser !== undefined) {
-        sessionLinks = (
-            <ul className='profileButtonUl'>
-                <li>
-                    <ProfileButton className={`profileButtonComponent`} sessionUser={sessionUser} />
-
-                </li>
-            </ul>
-        )
-    } else {
-        sessionLinks = (
-            <ul className='sessionLinksUl'>
-                <li>
-                    <button
-                        onClick={handleDemoLoginClick}
-                        className={`loginSignup`} >Demo Login</button>
-                    <button className={`loginSignup`} onClick={loginButton}> Login</button>
-                    <button className={`loginSignup`} onClick={signUpButton}> Sign Up</button>
-                </li>
-            </ul >
-        )
-    }
-
-    return (
-        <div className='mainavDiv'>
-            <ul className='navUl'>
-                <li className='homeButtonLi'>
-                    <NavLink className='homeButton' exact to={`/`}>Meetup</NavLink>
-                </li >
-                <li className='navLi'>
-                    {isLoaded && sessionLinks}
-                </li>
-            </ul>
-        </div>
+  const handleDemoLoginClick = () => {
+    return dispatch(
+      sessionActions.fetchLogin({
+        credential: "demoEdgar",
+        password: "password",
+      })
     )
+      .then(() => history.push("/"))
+      .then(() => window.location.reload());
+  };
+  if (sessionUser && sessionUser !== undefined) {
+    sessionLinks = (
+      <ul className="profileButtonUl">
+        <li>
+          <ProfileButton
+            className={`profileButtonComponent`}
+            sessionUser={sessionUser}
+          />
+        </li>
+      </ul>
+    );
+  } else {
+    sessionLinks = (
+      <ul className="sessionLinksUl">
+        <li>
+          <button
+            onClick={handleDemoLoginClick}
+            className={`loginSignup demoLogin underline-animation`}
+          >
+            Demo Login
+          </button>
+          <button
+            className={`loginSignup underline-animation`}
+            onClick={loginButton}
+          >
+            Login
+          </button>
+          <button
+            className={`loginSignup underline-animation`}
+            onClick={signUpButton}
+          >
+            Sign Up
+          </button>
+        </li>
+      </ul>
+    );
+  }
 
-
+  return (
+    <div className="mainNavDiv">
+      <ul className="navUl">
+        <li className="homeButtonLi">
+          <NavLink className="homeButton" exact to={`/`}>
+            Meetup
+          </NavLink>
+        </li>
+        <li className="navLi">{isLoaded && sessionLinks}</li>
+      </ul>
+    </div>
+  );
 }
 
-export default Navigation
+export default Navigation;
